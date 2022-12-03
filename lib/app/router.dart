d3ff8250b982825369411dbbe1304f53e7bc8872
JeffTyfoom <91708709+JeffTyfoom@users.noreply.github.com>
2022-12-04 06:28:07 +0800

    Removed unsued code

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -2,9 +2,6 @@ import 'package:auto_route/annotations.dart';
 import 'package:id_generator/features/page_layout/page_layout.dart';
 
 import '../features/page_initial_wrapper.dart';
-import '../features/page_sample_page.dart';
-import '../features/page_sample_page_2.dart';
-import '../features/page_sample_page_3.dart';
 
 @AdaptiveAutoRouter(
   routes: <AutoRoute>[
@@ -17,10 +14,6 @@ import '../features/page_sample_page_3.dart';
           page: PageLayout,
           initial: true,
         ),
-        AutoRoute(path: 'SamplePage3', page: SamplePage3),
-        AutoRoute(path: 'SamplePage', page: SamplePage, children: [
-          AutoRoute(path: '', page: SamplePage2, initial: true),
-        ])
       ],
     )
   ],
--- lib/features/page_layout/page_layout.dart
+++ lib/features/page_layout/page_layout.dart
@@ -1,12 +1,6 @@
-import 'dart:ui';
-
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart' hide Image;
 
 import '../id_painter/painter_widget.dart';
-import '../id_painter/model/shape.dart';
-
-typedef OnSave = Function(List<Shape>, Image?);
 
 class PageLayout extends StatefulWidget {
   const PageLayout({
@@ -20,17 +14,6 @@ class PageLayout extends StatefulWidget {
 class _PageLayoutState extends State<PageLayout> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sample '),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () => context.router.pushNamed('SamplePage'),
-          child: const Text('Click me'),
-        ),
-      ),
-    );
     return const Scaffold(
         body: Center(
       child: PainterWidget(
